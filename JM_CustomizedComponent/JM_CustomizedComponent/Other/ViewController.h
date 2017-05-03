5f3cd95d71c43080bb586cea0c8118a56868d58c
Jimmy <2846432477@qq.com>
2017-05-03 16:45:37 +0800

    created project

---
+++ JM_CustomizedComponent/JM_CustomizedComponent/Other/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JM_CustomizedComponent
+//
+//  Created by jimmy on 2017/5/3.
+//  Copyright © 2017年 Jimmy_Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JM_CustomizedComponent/JM_CustomizedComponent/Other/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JM_CustomizedComponent
+//
+//  Created by jimmy on 2017/5/3.
+//  Copyright © 2017年 Jimmy_Ng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
